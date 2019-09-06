7e7fe959aad31bf69c86d057a2f74c7845c7b538
Vincent Chan <okcdz@diverse.space>
2019-09-06 16:57:12 +0800

    feat: abstract parser_common

---
+++ src/parser/parser_common.cpp
@@ -0,0 +1,305 @@
+//
+// Created by Duzhong Chen on 2019/9/6.
+//
+
+#include "parser_common.h"
+
+namespace parser {
+
+    ParserCommon::ParserCommon(shared_ptr <std::u16string> source, const ParserCommon::Config& config):
+        source_(source), config_(config) {
+
+        error_handler_ = std::make_shared<ParseErrorHandler>();
+
+        scanner_ = make_unique<Scanner>(source, error_handler_);
+        scanner_->SetTrackComment(config_.comment);
+        has_line_terminator_ = false;
+
+        lookahead_.type_ = JsTokenType::EOF_;
+        lookahead_.line_number_ = scanner_->LineNumber();
+        lookahead_.line_start_ = 0;
+        lookahead_.range_ = make_pair(0, 0);
+
+        context_.is_module = false;
+        context_.await = false;
+        context_.allow_in = true;
+        context_.allow_strict_directive = true;
+        context_.allow_yield = true;
+        context_.is_assignment_target = false;
+        context_.is_binding_element = false;
+        context_.in_function_body = false;
+        context_.in_iteration = false;
+        context_.in_switch = false;
+        context_.strict_ = false;
+
+        start_marker_ = Marker {
+            0,
+            scanner_->LineNumber(),
+            0,
+        };
+
+        last_marker_ = Marker {
+            0,
+            scanner_->LineNumber(),
+            0,
+        };
+
+        NextToken();
+
+        last_marker_ = Marker {
+            scanner_->Index(),
+            scanner_->LineNumber(),
+            scanner_->Column(),
+        };
+    }
+
+    void ParserCommon::LogError(const std::string &message) {
+        auto index = last_marker_.index;
+        auto line = last_marker_.line;
+        auto column = last_marker_.column + 1;
+        error_handler_->CreateError(message, index, line, column);
+    }
+
+    void ParserCommon::UnexpectedToken(const Token *token, const std::string *message) {
+        string msg = "UnexpetedToken";
+        if (message) {
+            msg = *message;
+        }
+
+        UString value;
+        if (token) {
+            if (!message) {
+                msg = (token->type_ == JsTokenType::EOF_) ? "UnexpectedEOS" :
+                      (token->type_ == JsTokenType::Identifier) ? "UnexpectedIdentifier" :
+                      (token->type_ == JsTokenType::NumericLiteral) ? "UnexpectedNumber" :
+                      (token->type_ == JsTokenType::StringLiteral) ? "UnexpectedString" :
+                      (token->type_ == JsTokenType::Template) ? "UnexpectedTemplate" :
+                      "UnexpectedToken";
+
+                if (token->type_ == JsTokenType::Keyword) {
+                    if (Scanner::IsFutureReservedWord(token->value_)) {
+                        msg = "UnexpectedReserved";
+                    } else if (context_.strict_ && Scanner::IsStrictModeReservedWord(token->value_)) {
+                        msg = "StrictReservedWord";
+                    }
+                }
+            }
+
+            value = token->value_;
+        } else {
+            value = utils::To_UTF16("ILLEGAL");
+        }
+    }
+
+    void ParserCommon::DecorateToken(Token& token) {
+        token.loc_.start_ = Position {
+            start_marker_.line,
+            start_marker_.column,
+        };
+
+        token.loc_.end_ = Position {
+            scanner_->LineNumber(),
+            scanner_->Column(),
+        };
+    }
+
+    bool ParserCommon::NextToken(Token *result) {
+        Token token = lookahead_;
+
+        last_marker_.index = scanner_->Index();
+        last_marker_.line = scanner_->LineNumber();
+        last_marker_.column = scanner_->Column();
+
+        DO(CollectComments())
+
+        if (scanner_->Index() != start_marker_.index) {
+            start_marker_.index = scanner_->Index();
+            start_marker_.line = scanner_->LineNumber();
+            start_marker_.column = scanner_->Index() - scanner_->LineStart();
+        }
+
+        Token next;
+        DO(scanner_->Lex(next))
+
+        has_line_terminator_ = token.line_number_ != next.line_number_;
+
+        if (context_.strict_ && next.type_ == JsTokenType::Identifier) {
+            if (Scanner::IsStrictModeReservedWord(next.value_)) {
+                next.type_ = JsTokenType::Keyword;
+            }
+        }
+        lookahead_ = next;
+
+        if (config_.tokens && next.type_ != JsTokenType::EOF_) {
+            DecorateToken(next);
+            tokens_.push(next);
+        }
+
+        if (result) {
+            *result = token;
+        }
+
+        return true;
+    }
+
+    ParserCommon::Marker ParserCommon::CreateNode() {
+        return Marker {
+            start_marker_.index,
+            start_marker_.line,
+            start_marker_.column
+        };
+    }
+
+    ParserCommon::Marker ParserCommon::StartNode(Token &tok, std::uint32_t last_line_start) {
+        auto column = tok.range_.first - tok.line_start_;
+        auto line = tok.line_number_;
+        if (column < 0) {
+            column += last_line_start;
+            line--;
+        }
+        return Marker {
+            static_cast<uint32_t>(tok.range_.first),
+            line,
+            column
+        };
+    }
+
+    bool ParserCommon::Expect(char16_t t) {
+        Token token;
+        DO(NextToken(&token))
+        if (token.type_ != JsTokenType::Punctuator || token.value_.size() != 1 || token.value_[0] != t) {
+            UnexpectedToken(&token);
+            return false;
+        }
+        return true;
+    }
+
+    bool ParserCommon::Expect(const UString &keyword) {
+        Token token;
+        DO(NextToken(&token))
+        if (token.type_ != JsTokenType::Punctuator || token.value_.size() != 1 || token.value_ != keyword) {
+            UnexpectedToken(&token);
+            return false;
+        }
+        return true;
+    }
+
+    bool ParserCommon::ExpectCommaSeparator() {
+        return Expect(',');
+    }
+
+    bool ParserCommon::ExpectKeyword(const UString &keyword) {
+        Token token;
+        DO(NextToken(&token))
+
+        if (token.type_ != JsTokenType::Keyword || token.value_ != keyword) {
+            UnexpectedToken(&token);
+            return false;
+        }
+
+        return true;
+    }
+
+    bool ParserCommon::Match(char16_t t) {
+        Token token;
+        DO(NextToken(&token))
+        return token.type_ == JsTokenType::Punctuator && token.value_.size() == 1 && token.value_[0] == t;
+    }
+
+    bool ParserCommon::Match(const UString& t) {
+        Token token;
+        DO(NextToken(&token))
+        return token.type_ == JsTokenType::Punctuator && token.value_.size() == 1 && token.value_ == t;
+    }
+
+    bool ParserCommon::MatchKeyword(const UString &keyword) {
+        Token token;
+        DO(NextToken(&token))
+
+        return token.type_ == JsTokenType::Keyword && token.value_ == keyword;
+    }
+
+    bool ParserCommon::MatchAssign() {
+        if (lookahead_.type_ != JsTokenType::Punctuator) {
+            return false;
+        }
+        auto& op = lookahead_.value_;
+        return op == U("=") ||
+               op == U("*=") ||
+               op == U("**=") ||
+               op == U("/=") ||
+               op == U("%=") ||
+               op == U("+=") ||
+               op == U("-=") ||
+               op == U("<<=") ||
+               op == U(">>=") ||
+               op == U(">>>=") ||
+               op == U("&=") ||
+               op == U("^=") ||
+               op == U("|=");
+    }
+
+    bool ParserCommon::IsolateCoverGrammar(std::function<bool()> cb) {
+        auto previousIsBindingElement = context_.is_binding_element;
+        auto previousIsAssignmentTarget = context_.is_assignment_target;
+        auto previousFirstCoverInitializedNameError = context_.first_cover_initialized_name_error;
+
+        context_.is_binding_element = true;
+        context_.is_assignment_target = true;
+        context_.first_cover_initialized_name_error.reset();
+
+        bool result = cb();
+        if (context_.first_cover_initialized_name_error) {
+            LogError("firstCoverInitializedNameError");
+            return false;
+        }
+
+        context_.is_binding_element = previousIsBindingElement;
+        context_.is_assignment_target = previousIsAssignmentTarget;
+        context_.first_cover_initialized_name_error = previousFirstCoverInitializedNameError;
+
+        return result;
+    }
+
+    bool ParserCommon::InheritCoverGrammar(std::function<bool()> cb) {
+        auto previousIsBindingElement = context_.is_binding_element;
+        auto previousIsAssignmentTarget = context_.is_assignment_target;
+        auto previousFirstCoverInitializedNameError = context_.first_cover_initialized_name_error;
+
+        context_.is_binding_element = true;
+        context_.is_assignment_target = true;
+        context_.first_cover_initialized_name_error.reset();
+
+        bool result = cb();
+
+        context_.is_binding_element &= previousIsBindingElement;
+        context_.is_assignment_target &= previousIsAssignmentTarget;
+        context_.first_cover_initialized_name_error =
+            previousFirstCoverInitializedNameError ?
+            previousFirstCoverInitializedNameError : context_.first_cover_initialized_name_error;
+
+        return result;
+    }
+
+    bool ParserCommon::ConsumeSemicolon() {
+        if (Match(';')) {
+            return NextToken();
+        } else if (!has_line_terminator_) {
+            if (lookahead_.type_ != JsTokenType::EOF_ && !Match('}')) {
+                UnexpectedToken(&lookahead_);
+                return false;
+            }
+            last_marker_.index = start_marker_.index;
+            last_marker_.line = start_marker_.line;
+            last_marker_.column = start_marker_.column;
+        }
+        return true;
+    }
+
+    bool ParserCommon::CollectComments() {
+        DO(scanner_->ScanComments(comments_))
+
+        return true;
+    }
+
+}
---
+++ src/parser/parser_common.h
@@ -0,0 +1,121 @@
+//
+// Created by Duzhong Chen on 2019/9/6.
+//
+
+#pragma once
+
+#include <optional>
+#include <memory>
+#include <stack>
+#include <functional>
+#include "../tokenizer/token.h"
+#include "../parse_error_handler.h"
+#include "../utils.h"
+#include "../macros.h"
+#include "../tokenizer/scanner.h"
+#include "ast.h"
+
+namespace parser {
+    using namespace std;
+
+    static const char* ArrowParameterPlaceHolder = "ArrowParameterPlaceHolder";
+
+    class ParserCommon {
+    public:
+        struct Config {
+            bool range;
+            bool loc;
+            optional<UString> source;
+            bool tokens;
+            bool comment;
+            bool tolerant;
+        };
+
+        struct Context {
+            bool is_module;
+            bool allow_in;
+            bool allow_strict_directive;
+            bool allow_yield;
+            bool await;
+            optional<Token> first_cover_initialized_name_error;
+            bool is_assignment_target;
+            bool is_binding_element;
+            bool in_function_body;
+            bool in_iteration;
+            bool in_switch;
+            bool label_set;
+            bool strict_;
+        };
+
+        struct Marker {
+            uint32_t index = 0;
+            uint32_t line = 0;
+            uint32_t column = 0;
+        };
+
+        struct FormalParameterOptions {
+            bool simple = true;
+            vector<AstNode*> params;
+            bool stricted;
+            bool first_restricted;
+            string message;
+        };
+
+
+        ParserCommon(
+            shared_ptr<u16string> source,
+        const Config& config
+        );
+        ParserCommon(const ParserCommon&) = delete;
+        ParserCommon(ParserCommon&&) = delete;
+
+        ParserCommon& operator=(const ParserCommon&) = delete;
+        ParserCommon& operator=(ParserCommon&&) = delete;
+
+        void DecorateToken(Token& );
+
+        bool NextToken(Token* token = nullptr);
+        void LogError(const string& message);
+        void UnexpectedToken(const Token* tok = nullptr, const string* message = nullptr);
+
+        Marker CreateNode();
+        Marker StartNode(Token& tok, uint32_t last_line_start = 0);
+
+        bool Expect(char16_t t);
+        bool Expect(const UString& str);
+        bool ExpectCommaSeparator();
+        bool ExpectKeyword(const UString& keyword);
+
+        bool Match(char16_t t);
+        bool Match(const UString& str);
+        bool MatchKeyword(const UString& keyword);
+        bool MatchAssign();
+
+        bool IsolateCoverGrammar(std::function<bool()> cb);
+        bool InheritCoverGrammar(std::function<bool()> cb);
+
+        bool ConsumeSemicolon();
+
+        bool CollectComments();
+
+    protected:
+        Config config_;
+        Context context_;
+        Token lookahead_;
+        unique_ptr<Scanner> scanner_;
+
+        Marker start_marker_;
+        Marker last_marker_;
+
+        Sp<ParseErrorHandler> error_handler_;
+        Sp<std::u16string> source_;
+        bool has_line_terminator_;
+
+        stack<Token> tokens_;
+
+        vector<Comment> comments_;
+
+
+    };
+
+}
