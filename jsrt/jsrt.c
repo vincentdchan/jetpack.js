792046c456741ba7383fa12557b40132aea29a73
Vincent Chan <okcdz@diverse.space>
2020-06-30 22:42:31 +0800

    feat: add basic types
    - static str
    - arr
    - std object

--- jsrt/jsrt.c
+++ jsrt/jsrt.c
@@ -5,11 +5,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "jsrt.h"
 #include "./cutils.h"
 
 #define JSRT_HASHLIMIT      5
 #define STATIC_BUFFER_SIZE  4096
+#define STATIC_TRUE         (ctx->js_static->J_true)
+#define STATIC_FALSE        (ctx->js_static->J_false)
 
 #define CHECK_NOT_SMI(x) if (IS_SMI(x)) { \
         return JS_ERR_TYPE_MIS; \
@@ -17,32 +20,64 @@
 
 static uint8_t JSRT_StaticBuffer[STATIC_BUFFER_SIZE];
 
+static BOOL JSRT_StrEq(JSRT_CTX* ctx, JsStrCommon* val1, JsStrCommon* val2);
+
+static void ReleaseStr(JSRT_CTX* ctx, JsStrCommon* str);
+static void ReleaseArray(JSRT_CTX* ctx, JsArray* arr);
+static void ReleaseStdObject(JSRT_CTX* ctx, JsStdObject* std_obj);
+static inline JsObjectHead* RetainObj(JSRT_CTX* ctx, JsObjectHead* obj);
+static inline void ReleaseObj(JSRT_CTX* ctx, JsObjectHead* obj);
+
+JS_VAL JSRT_NewUndefined(JSRT_CTX* ctx, JS_FLAGS_RAW flags) {
+    JsObject* J_undefined = (JsObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsObject));
+    J_undefined->flags = JS_UNDEFINED | flags;
+    J_undefined->bool_val = 0;
+    J_undefined->rc = 1;
+    return PTR_TO_JS_VAL(J_undefined);
+}
+
+JS_VAL JSRT_NewBool(JSRT_CTX* ctx, JS_FLAGS_RAW flags, int val) {
+    JsObject* obj = (JsObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsObject));
+    obj->flags = JS_BOOL | flags;
+    obj->bool_val = val;
+    obj->rc = 1;
+    return PTR_TO_JS_VAL(obj);
+}
+
 JsStaticVal *NewJsStaticVal(struct JSRT_CTX* ctx) {
     JsStaticVal *sv = (JsStaticVal*)malloc(sizeof(JsStaticVal));
     memset(sv, 0, sizeof(JsStaticVal));
 
-    JsObject* J_undefined = (JsObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsObject));
-    J_undefined->flags = JS_UNDEFINED | JS_FLAGS_NO_GC;
-    J_undefined->bool_val = 0;
-    sv->J_undefined = PTR_TO_JS_VAL(J_undefined);
+    sv->J_undefined = JSRT_NewUndefined(ctx, JS_FLAGS_STATIC_VAL);
+    sv->J_true = JSRT_NewBool(ctx, JS_FLAGS_STATIC_VAL, TRUE);
+    sv->J_false = JSRT_NewBool(ctx, JS_FLAGS_STATIC_VAL, FALSE);
 
-    JsObject* J_true = (JsObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsObject));
-    J_true->flags = JS_BOOL | JS_FLAGS_NO_GC;
-    J_true->bool_val = TRUE;
-    sv->J_true = PTR_TO_JS_VAL(J_true);
+#define DEF(NAME, VALUE) \
+    sv->S_##NAME = JSRT_NewStrUTF8Auto(ctx, JS_FLAGS_STATIC_VAL, VALUE);
 
-    JsObject* J_false = (JsObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsObject));
-    J_true->flags = JS_BOOL | JS_FLAGS_NO_GC;
-    J_true->bool_val = FALSE;
-    sv->J_false = PTR_TO_JS_VAL(J_false);
+#include "static-str.h"
+
+#undef DEF
 
     return sv;
 }
 
+/**
+ * DO NOT use JSRT_Release, use JsHeap_Free directly
+ * to clear the heap
+ */
 void FreeJsStaticVal(struct JSRT_CTX* ctx, JsStaticVal* jsv) {
     JsHeap_Free(ctx->js_heap, (void*)JS_VAL_TO_PTR(jsv->J_undefined));
     JsHeap_Free(ctx->js_heap, (void*)JS_VAL_TO_PTR(jsv->J_true));
     JsHeap_Free(ctx->js_heap, (void*)JS_VAL_TO_PTR(jsv->J_false));
+
+#define DEF(NAME, VALUE) \
+    JsHeap_Free(ctx->js_heap, (void*)JS_VAL_TO_PTR(jsv->S_##NAME));
+
+#include "static-str.h"
+
+#undef DEF
+
     JSRT_FREE(jsv);
 }
 
@@ -66,14 +101,35 @@ void JSRT_FreeCtx(JSRT_CTX* ctx) {
     JSRT_FREE(ctx);
 }
 
-JS_VAL JSRT_NewSmi(JSRT_CTX* ctx, int32_t value) {
-    JS_VAL result = 0;
-    result = (JS_VAL)value << 1;
-    return result;
+JS_VAL JSRT_NewI32(JSRT_CTX* ctx, int32_t value) {
+    return I32_TO_SMI(value);
 }
 
-JS_VAL JSRT_NewStrUTF8(JSRT_CTX* ctx, const uint8_t* utf8) {
-    int str_size = strlen((const char*)utf8);
+static inline BOOL IsDoubleAInt(double value) {
+    return round(value) == value;
+}
+
+JS_VAL JSRT_NewDouble(JSRT_CTX* ctx, double value) {
+    if (IsDoubleAInt(value)) {
+        int64_t i64v = (int64_t)value;
+        if (i64v >= JSRT_MinSmiValue && i64v <= JSRT_MaxSmiValue) { // is smi
+            return JSRT_NewI32(ctx, (int32_t)i64v);
+        }
+    }
+    JsObject* obj = (JsObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsObject));
+    obj->flags = JS_NUM;
+    obj->rc = 1;
+    obj->num_val = value;
+
+    return PTR_TO_JS_VAL(obj);
+}
+
+JS_VAL JSRT_NewStrUTF8Auto(JSRT_CTX* ctx, JS_FLAGS_RAW flags, const char* utf8) {
+    uint32_t str_size = strlen(utf8);
+    return JSRT_NewStrUTF8(ctx, flags, (const uint8_t*)utf8, str_size);
+}
+
+JS_VAL JSRT_NewStrUTF8(JSRT_CTX* ctx, JS_FLAGS_RAW flags, const uint8_t* utf8, uint32_t str_size) {
     uint64_t size = sizeof(JsStdStr) + (str_size + 1) * sizeof(JS_CHAR_RAW);
     int* buffer = NULL;
     uint32_t buffer_size = 0;
@@ -108,11 +164,12 @@ JS_VAL JSRT_NewStrUTF8(JSRT_CTX* ctx, const uint8_t* utf8) {
     (*buffer_ptr) = 0;
     uint32_t unicode_size = buffer_ptr - buffer;
 
-    JsStdStr* obj = (JsStdStr*)JSRT_MALLOC(size);
+    JsStdStr* obj = (JsStdStr*)JsHeap_Allocate(ctx->js_heap, size);
 
-    obj->flags = JS_STR | JS_FLAGS_STD_STR;
+    obj->flags = JS_STR | JS_FLAGS_STD_STR | flags;
     obj->str_size = unicode_size;
     obj->simple_hash = JS_INVALID_STR_HASH;
+    obj->rc = 1;
     for (uint32_t i = 0; i < unicode_size; i++) {  // copy buffer
         obj->str_val[i] = buffer[i];
     }
@@ -128,7 +185,7 @@ JS_VAL JSRT_NewStrUTF8(JSRT_CTX* ctx, const uint8_t* utf8) {
 int JSRT_StdStrToUTF8(JSRT_CTX* ctx, JS_VAL val, uint8_t* bytes, uint32_t size) {
     CHECK_NOT_SMI(val)
 
-    uint64_t obj_ptr = JS_VAL_TO_PTR(val);
+    JS_PTR obj_ptr = JS_VAL_TO_PTR(val);
 
     if (JS_TYPE(obj_ptr) != JS_STR) {
         return JS_ERR_TYPE_MIS;
@@ -158,6 +215,150 @@ int JSRT_StdStrToUTF8(JSRT_CTX* ctx, JS_VAL val, uint8_t* bytes, uint32_t size) 
 }
 #undef DISTANCE
 
+uint16_t JsStrDepth(JsStrCommon* str) {
+    if ((str->flags & JS_STR_FLAGS_TYPE) == JS_FLAGS_STD_STR) {
+        return 1;
+    }
+
+    JsConcatStr *concat_str = (JsConcatStr*)str;
+    return concat_str->depth;
+}
+
+uint16_t JsStrNodeSize(JsStrCommon* str) {
+    if ((str->flags & JS_STR_FLAGS_TYPE) == JS_FLAGS_STD_STR) {
+        return 1;
+    }
+
+    JsConcatStr *concat_str = (JsConcatStr*)str;
+    return concat_str->node_size;
+}
+
+static inline JsStrCommon* NormalizeConcatStr(JSRT_CTX* ctx, JsConcatStr* input) {
+    return (JsStrCommon*)input;
+}
+
+JsStrCommon* JSRT_ConcatStr(JSRT_CTX* ctx, JsStrCommon* left, JsStrCommon* right) {
+    uint32_t bytes_size = sizeof(JsConcatStr);
+    JsConcatStr* str = (JsConcatStr*)malloc(bytes_size);
+    memset(str, 0, bytes_size);
+
+    str->flags = JS_STR | JS_FLAGS_CONCAT_STR;
+    str->rc = 1;
+    str->str_size = left->str_size + right->str_size;
+    str->simple_hash = JS_INVALID_STR_HASH;
+
+    uint32_t left_depth = JsStrDepth(left);
+    uint32_t right_depth = JsStrDepth(right);
+    str->depth = (uint16_t)(max_uint32(left_depth, right_depth) + 1);
+
+    uint32_t left_node_size = JsStrNodeSize(left);
+    uint32_t right_node_size = JsStrNodeSize(right);
+    str->node_size = left_node_size + right_node_size;
+
+    // increase counter
+    RetainObj(ctx, (JsObjectHead*)left);
+    RetainObj(ctx, (JsObjectHead*)right);
+
+    str->left = left;
+    str->right = right;
+
+    return NormalizeConcatStr(ctx, str);
+}
+
+static void ReleaseStr(JSRT_CTX* ctx, JsStrCommon* str) {
+    if ((str->flags & JS_STR_FLAGS_TYPE) == JS_FLAGS_STD_STR) {
+        JsHeap_Free(ctx->js_heap, str);
+        return;
+    }
+
+    JsConcatStr* concat_str = (JsConcatStr*)str;
+
+    ReleaseObj(ctx, (JsObjectHead*)concat_str->left);
+    ReleaseObj(ctx, (JsObjectHead*)concat_str->right);
+
+    JsHeap_Free(ctx->js_heap, str);
+}
+
+JS_VAL JSRT_NewArray(JSRT_CTX* ctx, JS_FLAGS_RAW flags, uint32_t cap) {
+    uint32_t bytes_size = sizeof(JsArray) + sizeof(JS_VAL) * cap;
+    JsArray* arr = (JsArray*)JsHeap_Allocate(ctx->js_heap, bytes_size);
+    memset(arr, 0 , bytes_size);
+
+    arr->flags = JS_ARR | flags;
+    arr->rc = 1;
+    arr->size = 0;
+    arr->capacity = cap;
+
+    return PTR_TO_JS_VAL(arr);
+}
+
+static void ReleaseArray(JSRT_CTX* ctx, JsArray* arr) {
+    for (uint32_t i = 0; i < arr->size; i++) {
+        JSRT_Release(ctx, arr->data[i]);
+    }
+    JsHeap_Free(ctx->js_heap, arr);
+}
+
+JS_VAL JSRT_NewStdObject(JSRT_CTX* ctx, JS_FLAGS_RAW flags) {
+    JsStdObject* std_obj = (JsStdObject*)JsHeap_Allocate(ctx->js_heap, sizeof(JsStdObject));
+
+    std_obj->flags = JS_OBJ | flags;
+    std_obj->rc = 1;
+    std_obj->prop_size = 0u;
+    std_obj->bucket_cap = JS_OBJ_BUCKET_CAP;
+    std_obj->start_prop = NULL;
+    std_obj->end_prop = NULL;
+    std_obj->bucket = NULL;
+
+    return PTR_TO_JS_VAL(std_obj);
+}
+
+/**
+ * Clean jobs:
+ * 1. decrease ref of (key, value)
+ * 2. clean all the bucket
+ * 3. clean the bucket array
+ * 4. free std_obj itself
+ *
+ * start_propt* -> prop1
+ *                   |
+ *                 prop2
+ *                   |
+ * end_prop*    -> prop3
+ *
+ * bucket** ->
+ *   [0] NULL
+ *   [1] -> bucket1
+ *   [2] NULL
+ *   [3] -> bucket1 -> bucket2 -> bucket3
+ */
+static void ReleaseStdObject(JSRT_CTX* ctx, JsStdObject* std_obj) {
+    JsStdObjectProp* ptr = std_obj->start_prop;
+    while (ptr != std_obj->end_prop) {
+        JSRT_Release(ctx, ptr->key);
+        JSRT_Release(ctx, ptr->value);
+        ptr = ptr->next;
+    }
+
+    if (std_obj->bucket != NULL) {
+        // free all the buckets on the list
+        for (uint32_t i = 0; i < std_obj->bucket_cap; i++) {
+            JsStdObjectPropBucket* bucket_ptr = std_obj->bucket[i].next;
+
+            while (bucket_ptr != NULL) {
+                JsStdObjectPropBucket* next = bucket_ptr->next;
+
+                JsHeap_Free(ctx->js_heap, bucket_ptr);
+
+                bucket_ptr = next;
+            }
+        }
+        JsHeap_Free(ctx->js_heap, std_obj->bucket);
+    }
+
+    JsHeap_Free(ctx->js_heap, std_obj);
+}
+
 static inline void __StdStrCalSimpleHashIfNotExist(JSRT_CTX* ctx, JsStdStr* std_str) {
     if (std_str->simple_hash == JS_INVALID_STR_HASH) {
         std_str->simple_hash = JSRT_SimpleHash(std_str->str_val, std_str->str_size, ctx->seed);
@@ -183,17 +384,43 @@ not_implemented:
     return JS_INVALID_PTR;
 }
 
-JS_VAL JSRT_StrEq(JSRT_CTX * ctx, JS_VAL val1, JS_VAL val2) {
-    uint64_t str1 = JS_VAL_TO_PTR(val1);
-    uint64_t str2 = JS_VAL_TO_PTR(val2);
+JS_VAL JSRT_StrictEq(JSRT_CTX* ctx, JS_VAL val1, JS_VAL val2) {
+    if (IS_SMI(val1) && IS_SMI(val2)) {
+        if (SMI_TO_I32(val1) == SMI_TO_I32(val2)) {
+            return STATIC_TRUE;
+        } else {
+            return STATIC_FALSE;
+        }
+    }
 
+    JS_PTR p1 = JS_VAL_TO_PTR(val1);
+    JS_PTR p2 = JS_VAL_TO_PTR(val2);
+
+    JS_FLAGS_RAW t1 = PTR_TYPE(p1);
+    JS_FLAGS_RAW t2 = PTR_TYPE(p2);
+
+    BOOL result;
+    if (t1 == JS_STR && t2 == JS_STR) {
+        JsStrCommon *str1 = (JsStrCommon *) p1;
+        JsStrCommon *str2 = (JsStrCommon *) p2;
+        result = JSRT_StrEq(ctx, str1, str2);
+    } else if (t1 == JS_NUM && t2 == JS_NUM) {
+        result = (((JsObject*)p1)->num_val == ((JsObject*)p2)->num_val);
+    } else {
+        result = (p1 == p2);
+    }
+
+    return result ? STATIC_TRUE : STATIC_FALSE;
+}
+
+static BOOL JSRT_StrEq(JSRT_CTX * ctx, JsStrCommon* val1, JsStrCommon* val2) {
     if (JS_STR_TYPE(val1) == JS_FLAGS_STD_STR && JS_STR_TYPE(val2) == JS_FLAGS_STD_STR) {
 
-        JsStdStr *std_str1 = (JsStdStr*)str1;
-        JsStdStr *std_str2 = (JsStdStr*) str2;
+        JsStdStr *std_str1 = (JsStdStr*)val1;
+        JsStdStr *std_str2 = (JsStdStr*)val2;
 
         if (std_str1->str_size != std_str2->str_size) {
-            return ctx->js_static->J_false;
+            return TRUE;
         }
 
         __StdStrCalSimpleHashIfNotExist(ctx, std_str1);
@@ -201,20 +428,19 @@ JS_VAL JSRT_StrEq(JSRT_CTX * ctx, JS_VAL val1, JS_VAL val2) {
 
         // if hash is different, it's different
         if (std_str1->simple_hash != std_str2->simple_hash) {
-            return ctx->js_static->J_false;
+            return FALSE;
         }
 
         for (uint32_t i = 0; i < std_str1->str_size; i++) {
             if (std_str1->str_val[i] != std_str2->str_val[i]) {
-                return ctx->js_static->J_false;
+                return FALSE;
             }
         }
 
-        return ctx->js_static->J_true;
+        return TRUE;
     }
 
-    JSRT_Errno = JS_ERR_NOT_IMPLEMENTED;
-    return JS_INVALID_PTR;
+    return FALSE;
 }
 
 // copy from lua source code
@@ -224,4 +450,57 @@ uint32_t JSRT_SimpleHash(const JS_CHAR_RAW *str, size_t l, uint32_t seed) {
     for (; l >= step; l -= step)
         h ^= ((h<<5) + (h>>2) + str[l - 1]);
     return h;
+}
+
+JS_RC_RAW JSRT_GetRetainCount(JS_VAL val) {
+    if (IS_SMI(val)) {
+        return 0;
+    }
+    JsObjectHead* obj = (JsObjectHead*)JS_VAL_TO_PTR(val);
+    return obj->rc;
+}
+
+JS_VAL JSRT_Retain(JSRT_CTX* ctx, JS_VAL val) {
+    if (IS_SMI(val)) {
+        return val;
+    }
+    JsObjectHead* obj = (JsObjectHead*)JS_VAL_TO_PTR(val);
+    RetainObj(ctx, obj);
+    return val;
+}
+
+void JSRT_Release(JSRT_CTX* ctx, JS_VAL val) {
+    if (IS_SMI(val)) {
+        return;
+    }
+    JsObjectHead* obj = (JsObjectHead*)JS_VAL_TO_PTR(val);
+    RetainObj(ctx, obj);
+}
+
+static inline JsObjectHead* RetainObj(JSRT_CTX* ctx, JsObjectHead* obj) {
+    obj->rc++;
+    return obj;
+}
+
+static inline void ReleaseObj(JSRT_CTX* ctx, JsObjectHead* obj) {
+    if (--obj->rc == 0) {
+        JS_FLAGS_RAW obj_type = obj->flags & JS_OBJ_FLAGS_TYPE;
+        switch (obj_type) {
+            case JS_STR:
+                ReleaseStr(ctx, (JsStrCommon*)obj);
+                break;
+
+            case JS_ARR:
+                ReleaseArray(ctx, (JsArray*)obj);
+                break;
+
+            case JS_OBJ:
+                ReleaseStdObject(ctx, (JsStdObject *) obj);
+                break;
+
+            default:
+                JsHeap_Free(ctx->js_heap, obj);
+
+        }
+    }
 }
--- jsrt/jsrt.h
+++ jsrt/jsrt.h
@@ -11,12 +11,18 @@
 
 #include "jsheap.h"
 
+#define JS_ARR_DEFAULT_CAP   4
+#define JS_SMI_OFFSET        32u
+#define JS_OBJ_BUCKET_CAP    4
 #define JS_DOUBLE_RAW        double
 #define JS_CHAR_RAW          unsigned int
 #define JS_FLAGS_RAW         uint32_t
 #define JS_RC_RAW            uint32_t
 #define JS_INVALID_PTR       ((uint64_t)-1)
 #define JS_INVALID_STR_HASH  ((uint32_t)-1)
+
+#define JS_VAL               uint64_t
+#define JS_PTR               JsObjectHead*
 
 typedef enum : unsigned int {
     JS_UNDEFINED = 0u,
@@ -34,13 +40,14 @@ typedef enum : int {
     JS_ERR_NOT_IMPLEMENTED = -4,
 } JS_ERR;
 
-#define JS_VAL uint64_t
+struct JSRT_CTX;
+struct JsObjectHead;
 
-#define IS_SMI(x) (((JS_VAL)x & 1u) == 0)
-#define JS_VAL_TO_PTR(x) ((JS_VAL)x & (~1u))
-#define PTR_TO_JS_VAL(x) ((JS_VAL)x | 1u)
-#define SMI_TO_I32(x) ((int32_t)((JS_VAL)x >> 1u))
-#define I32_TO_SMI(x) ((JS_VAL)x << 1u)
+#define IS_SMI(x) (((JS_VAL)(x) & 1u) == 0)
+#define JS_VAL_TO_PTR(x) ((JS_PTR)((x) & (~1u)))
+#define PTR_TO_JS_VAL(x) ((JS_VAL)(x) | 1u)
+#define SMI_TO_I32(x) ((int32_t)((JS_VAL)(x) >> JS_SMI_OFFSET))
+#define I32_TO_SMI(x) ((JS_VAL)(x) << JS_SMI_OFFSET)
 
 static const int64_t JSRT_MaxSmiValue = 0x7FFFFFFF;
 static const int64_t JSRT_MinSmiValue = 0xFFFFFFFF;
@@ -52,35 +59,113 @@ static const int64_t JSRT_MinSmiValue = 0xFFFFFFFF;
 #define JS_FLAGS_CONCAT_STR  0b00000000000000000000000100000000u
 
 #define JS_GC_FLAGS_TYPE     0b11110000000000000000000000000000u
-#define JS_FLAGS_NO_GC       0b00010000000000000000000000000000u
+#define JS_FLAGS_STATIC_VAL  0b00010000000000000000000000000000u
 
-struct JSRT_CTX;
+typedef struct {
+    JS_FLAGS_RAW flags;
+    JS_RC_RAW rc;
+} JsObjectHead;
 
-enum JS_STR_TYPE {
-    JS_STR_STD = 0,
-    JS_STR_CONCATED = 1,
-};
+#define PTR_TYPE(x) (x->flags & JS_OBJ_FLAGS_TYPE)
+
+// String
 
 typedef struct {
     JS_FLAGS_RAW flags;
     JS_RC_RAW rc;
     uint32_t      str_size;
     uint32_t      simple_hash;
+} JsStrCommon ;
+
+uint16_t JsStrDepth(JsStrCommon* str);
+uint16_t JsStrNodeSize(JsStrCommon* str);
+
+typedef struct {
+    JS_FLAGS_RAW flags;
+    JS_RC_RAW rc;
+    uint32_t      str_size;
+    uint32_t      simple_hash;
+    // str common end
+
     JS_CHAR_RAW   str_val[0];
 } JsStdStr;
 
-#define JS_STR_TYPE(x) (((JsStdStr*)x)->flags & JS_STR_FLAGS_TYPE)
+/**
+ * flat array of JsStdStr,
+ * good at quick concat,
+ * TODO: make it a rope tree
+ */
+typedef struct {
+    JS_FLAGS_RAW  flags;
+    JS_RC_RAW     rc;
+    uint32_t      str_size;
+    uint32_t      simple_hash;
+    // str common end
 
-JsStdStr NewJsStrByCstr(struct JSRT_CTX* ctx, const char* bytes);
+    uint16_t      node_size;
+    uint16_t      depth;
+    JsStrCommon*  left;
+    JsStrCommon*  right;
+} JsConcatStr;
+
+#define JS_STR_TYPE(x) (((JsObjectHead*)x)->flags & JS_STR_FLAGS_TYPE)
+
+// Array
+
+typedef struct {
+    JS_FLAGS_RAW  flags;
+    JS_RC_RAW     rc;
+    uint32_t      size;
+    uint32_t      capacity;
+    JS_VAL        data[0];
+} JsArray;
+
+// Static Values
+
+#define DEF(NAME, VAL) JS_VAL S_##NAME;
 
 typedef struct {
     JS_VAL J_undefined;
     JS_VAL J_true;
     JS_VAL J_false;
+
+#include "static-str.h"
+
 } JsStaticVal;
+
+#undef DEF
 
 JsStaticVal *NewJsStaticVal(struct JSRT_CTX* ctx);
 void FreeJsStaticVal(struct JSRT_CTX* ctx, JsStaticVal* jsv);
+
+// StandardObject
+
+typedef struct JsStdObjectProp {
+    JS_VAL key;
+    JS_VAL value;
+    struct JsStdObjectProp* next;
+} JsStdObjectProp;
+
+// 16 bytes, is a smo
+typedef struct JsStdObjectPropBucket {
+    JsStdObjectProp* prop;
+    struct JsStdObjectPropBucket* next;
+} JsStdObjectPropBucket;
+
+typedef struct {
+    JS_FLAGS_RAW flags;
+    JS_RC_RAW rc;
+    // 8 bytes
+    uint32_t prop_size;
+    uint32_t bucket_cap;
+    // 16 bytes
+    JsStdObjectProp* start_prop;
+    JsStdObjectProp* end_prop;
+    // 32 bytes
+    JsStdObjectPropBucket* bucket;
+} JsStdObject;
+
+// Object
 
 typedef struct {
     JS_FLAGS_RAW flags;
@@ -88,7 +173,6 @@ typedef struct {
     union {
         int32_t       bool_val;
         JS_DOUBLE_RAW num_val;
-        JsStdStr*     std_str_val;
     };
 } JsObject;
 
@@ -103,14 +187,27 @@ typedef struct JSRT_CTX {
 JSRT_CTX* JSRT_NewCtx();
 void JSRT_FreeCtx(JSRT_CTX* ctx);
 
-JS_VAL JSRT_NewSmi(JSRT_CTX* ctx, int32_t value);
-JS_VAL JSRT_NewStrUTF8(JSRT_CTX* ctx, const uint8_t* utf8);
+JS_VAL JSRT_NewUndefined(JSRT_CTX* ctx, JS_FLAGS_RAW flags);
+JS_VAL JSRT_NewBool(JSRT_CTX* ctx, JS_FLAGS_RAW flags, int val);
+JS_VAL JSRT_NewI32(JSRT_CTX* ctx, int32_t value);
+JS_VAL JSRT_NewDouble(JSRT_CTX*ctx, double value);
+JS_VAL JSRT_NewStrUTF8Auto(JSRT_CTX* ctx, JS_FLAGS_RAW flags, const char* utf8);
+JS_VAL JSRT_NewStrUTF8(JSRT_CTX* ctx, JS_FLAGS_RAW flags, const uint8_t* utf8, uint32_t str_size);
 int JSRT_StdStrToUTF8(JSRT_CTX* ctx, JS_VAL val, uint8_t* bytes, uint32_t size);
 
+JS_VAL JSRT_NewArray(JSRT_CTX* ctx, JS_FLAGS_RAW flags, uint32_t cap);
+
+JS_VAL JSRT_NewStdObject(JSRT_CTX* ctx, JS_FLAGS_RAW flags);
+JS_VAL JSRT_StdObjectGetVal(JSRT_CTX* ctx, JsStdObject* std_obj, JS_VAL key);
+
+JS_VAL JSRT_StrictEq(JSRT_CTX* ctx, JS_VAL val1, JS_VAL val2);
 JS_VAL JSRT_ValAdd(JSRT_CTX* ctx, JS_VAL val1, JS_VAL val2);
-JS_VAL JSRT_StrEq(JSRT_CTX* ctx, JS_VAL val1, JS_VAL val2);
 
 uint32_t JSRT_SimpleHash(const JS_CHAR_RAW *str, size_t l, uint32_t seed);
+
+JS_RC_RAW JSRT_GetRetainCount(JS_VAL val);
+JS_VAL JSRT_Retain(JSRT_CTX* ctx, JS_VAL val);
+void JSRT_Release(JSRT_CTX* ctx, JS_VAL val);
 
 static int JSRT_Errno;
 
