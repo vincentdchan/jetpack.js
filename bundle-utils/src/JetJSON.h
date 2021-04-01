6160826bc418d6cfb6c91e89f066b9d6ba9c3ee8
Vincent Chan <okcdz@diverse.space>
2021-04-01 13:14:19 +0800

    feat: json escape

---
+++ bundle-utils/src/JetJSON.h
@@ -0,0 +1,16 @@
+//
+// Created by chenduzhong on 2021/4/1.
+//
+
+#ifndef ROCKET_BUNDLE_JETJSON_H
+#define ROCKET_BUNDLE_JETJSON_H
+
+#include <string>
+
+namespace jetpack {
+
+    std::string EscapeJSONString(const std::string& str);
+
+}
+
+#endif //ROCKET_BUNDLE_JETJSON_H
